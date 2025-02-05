aba67049563721acd56207367b348f8219a3de16
iEwha <royiewha@gmail.com>
2025-02-05 09:42:12 +0800

    -

--- Script/TikTok.js
+++ Script/TikTok.js
@@ -1,65 +1,74 @@
-var watermark = body => {
+const watermark = (body) => {
     try {
-        body = body.replace(/\"room_id\":(\d{2,})/g, '"room_id":"$1"');
-        let obj = JSON.parse(body);
-        if (obj.data) obj.data = Follow(obj.data);
-        if (obj.aweme_list) obj.aweme_list = Feed(obj.aweme_list);
-        if (obj.aweme_detail) obj.aweme_detail = Share(obj.aweme_detail);
-        if (obj.aweme_details) obj.aweme_details = Feed(obj.aweme_details);
-        $done({ body: JSON.stringify(obj) });
-    } catch (err) {
-        console.log("Error:\n" + err);
-        $done({});
-    }
-}
-watermark($response.body);
-
-function Follow(data) {
-    if (data && Array.isArray(data)) {
-        for (let item of data) {
-            if (item.aweme?.video) video_lists(item.aweme);
+        if (typeof body === "string") {
+            body = body.replace(/\"room_id\":(\d{2,})/g, '"room_id":"$1"');
+            body = JSON.parse(body);
         }
+
+        if (body.data) body.data = processFollowData(body.data);
+        if (body.aweme_list) body.aweme_list = processFeedData(body.aweme_list);
+        if (body.aweme_detail) body.aweme_detail = processShareData(body.aweme_detail);
+        if (body.aweme_details) body.aweme_details = processFeedData(body.aweme_details);
+
+        $done({ body: JSON.stringify(body) });
+    } catch (err) {
+        console.error("Error occurred:\n", err);
+        $done({ body: JSON.stringify(body) });
     }
+};
+
+function processFollowData(data) {
+    if (!Array.isArray(data)) return data;
+
+    data.forEach((item) => {
+        if (item.aweme?.video) processVideoData(item.aweme);
+    });
+
     return data;
 }
 
-function Feed(aweme_list) {
-    if (aweme_list && Array.isArray(aweme_list)) {
-        for (let i = aweme_list.length - 1; i >= 0; i--) {
-            if (aweme_list[i].is_ads) {
-                aweme_list.splice(i, 1);
-            } else if (aweme_list[i].video) {
-                video_lists(aweme_list[i]);
-            }
+function processFeedData(awemeList) {
+    if (!Array.isArray(awemeList)) return awemeList;
+    const enabled_live = false;
+
+    return awemeList.filter((item) => {
+        if (item.is_ads) return false;
+        if (item.video) {
+            processVideoData(item);
+            return true;
         }
-    }
-    return aweme_list;
+        return enabled_live;
+    });
 }
 
-function Share(aweme_detail) {
-    if (aweme_detail.video) video_lists(aweme_detail);
-    return aweme_detail;
+function processShareData(awemeDetail) {
+    if (awemeDetail?.video) processVideoData(awemeDetail);
+    return awemeDetail;
 }
 
-function video_lists(lists) {
-    lists.prevent_download = false;
-    lists.status.reviewed = 1;
-    lists.video_control.allow_download = true;
-    lists.video_control.prevent_download_type = 0;
-    delete lists.video.misc_download_addrs;
-    lists.video.download_addr = lists.video.play_addr;
-    lists.video.download_suffix_logo_addr = lists.video.play_addr;
-    lists.aweme_acl.download_general.mute = false;
-    
-    if (lists.aweme_acl.download_general.extra) {
-        delete lists.aweme_acl.download_general.extra;
-        Object.assign(lists.aweme_acl.download_general, {
+function processVideoData(videoItem) {
+    if (!videoItem.video || !videoItem.video_control) return;
+
+    videoItem.prevent_download = false;
+    videoItem.video_control.allow_download = true;
+    videoItem.video_control.prevent_download_type = 0;
+
+    videoItem.video.download_addr = videoItem.video.play_addr;
+    videoItem.video.download_suffix_logo_addr = videoItem.video.play_addr;
+
+    delete videoItem.video.misc_download_addrs;
+
+    if (videoItem.aweme_acl?.download_general) {
+        Object.assign(videoItem.aweme_acl.download_general, {
+            mute: false,
             code: 0,
             show_type: 2,
-            transcode: 3
+            transcode: 3,
         });
-        lists.aweme_acl.download_mask_panel = lists.aweme_acl.download_general;
-        lists.aweme_acl.share_general = lists.aweme_acl.download_general;
+
+        videoItem.aweme_acl.download_mask_panel = videoItem.aweme_acl.download_general;
+        videoItem.aweme_acl.share_general = videoItem.aweme_acl.download_general;
     }
-    return lists;
 }
+
+watermark($response.body);
