426ddac522c60c979a4e632acd89f344dd858126
iEwha <81971510+iEwha@users.noreply.github.com>
2025-02-06 17:13:27 +0800

    -

--- Script/photoshop.js
+++ Script/photoshop.js
@@ -1,6 +1,9 @@
-let obj = JSON.parse($response.body)
-let pro= obj["mobileProfile"];
-pro["profileStatus"] = "PROFILE_AVAILABLE";
-pro["legacyProfile"] = "{}";
-pro["relationshipProfile"] = "[]";
-$done({body: JSON.stringify(obj)})
+let obj = JSON.parse($response.body);
+if (obj.hasOwnProperty("mobileProfile") && typeof obj.mobileProfile === "object") {
+    let pro = obj.mobileProfile;
+    pro.profileStatus = "PROFILE_AVAILABLE";
+    pro.legacyProfile = {};  
+    pro.relationshipProfile = [];  
+}
+
+$done({ body: JSON.stringify(obj) });
