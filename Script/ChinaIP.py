04559996a40299fce1e7b820e86718e8d58398cf
iEwha <81971510+iEwha@users.noreply.github.com>
2026-04-16 20:07:48 +0800

    Update ChinaIP.py

--- Script/ChinaIP.py
+++ Script/ChinaIP.py
@@ -1,25 +1,18 @@
 import requests
-
-China = "https://raw.githubusercontent.com/cbuijs/ipasn/master/country-asia-china4.list"
-r = requests.get(China) 
-with open("Provider/ChinaIP.list", "wb") as ip:
-         ip.write(r.content)
-
-ipv4 = open('Provider/Ruleset/cncidr.list','w')  
-with open('Provider/ChinaIP.list','r') as ip:
-    line = ip.readlines()     
-    for line_list in line:    
-        line_new = 'IP-CIDR,' + line_list  
-        ipv4.write(line_new)  
-
-Clashipv4 = open('Provider/Ruleset/cncidr.yaml','w')  
-with open('Provider/ChinaIP.list','r') as ip:
-    Clashipv4.write("payload: \n")
-    line = ip.readlines()     
-    for line_list in line:    
-      line_new = '  - IP-CIDR,' + line_list 
-      Clashipv4.write(line_new)  
-
+url = "https://raw.githubusercontent.com/cbuijs/ipasn/master/country-asia-china4.list"
+resp = requests.get(url)
+resp.raise_for_status()          
+with open("Provider/ChinaIP.list", "wb") as f:
+    f.write(resp.content)
+with open('Provider/Ruleset/cncidr.list', 'w') as f:
+    with open('Provider/ChinaIP.list', 'r') as src:
+        for line in src:
+            f.write(f'IP-CIDR,{line}')
+with open('Provider/Ruleset/cncidr.yaml', 'w') as f:
+    f.write("payload:\n")
+    with open('Provider/ChinaIP.list', 'r') as src:
+        for line in src:
+            f.write(f'  - IP-CIDR,{line}')
  
 
 
