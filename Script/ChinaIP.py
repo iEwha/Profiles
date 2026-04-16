7f1c929162dd321238dda529be1afea961c61335
iEwha <royiewha@gmail.com>
2026-04-16 21:16:31 +0800

    +

--- Script/ChinaIP.py
+++ Script/ChinaIP.py
@@ -1,18 +1,24 @@
 import requests
+
 url = "https://raw.githubusercontent.com/cbuijs/ipasn/master/country-asia-china4.list"
+
 resp = requests.get(url)
-resp.raise_for_status()          
-with open("Provider/ChinaIP.list", "wb") as f:
-    f.write(resp.content)
-with open('Provider/Ruleset/cncidr.list', 'w') as f:
-    with open('Provider/ChinaIP.list', 'r') as src:
-        for line in src:
-            f.write(f'IP-CIDR,{line}')
-with open('Provider/Ruleset/cncidr.yaml', 'w') as f:
+resp.raise_for_status()   
+
+lines = resp.text.strip().splitlines()
+
+with open('Provider/Ruleset/cncidr.list', 'w', encoding='utf-8') as f:
+    for line in lines:
+        if line.strip():                     
+            f.write(f'IP-CIDR,{line}\n')
+
+with open('Provider/Ruleset/cncidr.yaml', 'w', encoding='utf-8') as f:
     f.write("payload:\n")
-    with open('Provider/ChinaIP.list', 'r') as src:
-        for line in src:
-            f.write(f'  - IP-CIDR,{line}')
+    for line in lines:
+        if line.strip():
+            f.write(f'  - IP-CIDR,{line}\n')
+
+
  
 
 
